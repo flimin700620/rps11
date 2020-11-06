9f8a3fc1ce71b42e82ca246243b78910be184054
testremote <you@example.com>
2020-11-06 03:55:24 +0000

    CICD v1

--- src/test/java/com/xp/rps/RpsRepoTest.java
+++ src/test/java/com/xp/rps/RpsRepoTest.java
@@ -3,27 +3,38 @@ package com.xp.rps;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@JdbcTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@Import(JDBCRepo.class) //Somehow @JdbcTest not able to load @Repository, Import to force it happen
+@Transactional //Default with @JDBCTest This will cause each test rollback after testing, also rollback the beforeTestRun.sql
 public class RpsRepoTest {
 
 
+    @Autowired
     RpsRepo repo;
 
     @BeforeEach
     void setup() {
-        repo = new InMemoryRepo();
+        //repo = new InMemoryRepo();
     }
 
 
     @Test
+    //@Rollback(value = false)
     void createGame() {
         Game g = new Game("Ray", "James", 3, "Buy a coffee machine");
         int gameID=repo.createGame(g);
 
         Game g2 = repo.getGame(gameID);
+        System.out.println("=============>"+g2.getId());
         assertEquals(g.getPlayer1(), g2.getPlayer1());
 
         Round r1 = new Round(Throw.ROCK, Throw.PAPER, Result.P2_WINS);
